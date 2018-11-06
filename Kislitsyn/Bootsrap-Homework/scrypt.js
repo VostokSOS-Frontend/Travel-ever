19df68394d2fe7e9883bf5fd33314328639bf21b
Daniil Kislitsyn <dania.drug.gamer56@gmail.com>
2018-11-06 19:55:44 +0300

    A lot of

---
+++ Kislitsyn/Bootsrap-Homework/scrypt.js
@@ -0,0 +1,16 @@
+// function copyObj(sourse) {
+//     let copy = {};
+//     let keys = Object.keys(sourse);
+//     keys.forEach(key => {
+//         if (typeof sourse[key] === "object") {
+//             copy[key] = copyObj(sourse[key])
+//         } else {
+//             copy[key] = sourse[key];
+//         }
+//     });
+//     return copy;
+// } 
+
+// let text = 
+// 5*(F-32)/9
+// let re = /[=-]?\d+(F)/
